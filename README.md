95a6138440953e464369d02e007b649f3bc91f8e
Diego Vergara Casanova <davc93@gmail.com>
2022-10-22 13:33:59 -0300

    Se agrega configuracion de gh-pages

---
+++ README.md
@@ -0,0 +1,3 @@
+# Travels
+
+El siguiente proyecto es una replica del curso de platzi tailwindcss 2 y 3 
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import './style.css'
-
 function toggleDarkmode (){
     document.querySelector('html')?.classList.toggle('dark')
 }
